25ecbb0cb8a00973cca727bca456ecd526dbd724
Seth Keel <seth.keel@gmail.com>
2016-07-24 18:32:15 -0400

    second commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
-//Set global require variable for mysql and console.table NPM packages
+//Set global require variable for mysql, console.table, and inquirer NPM packages
 var mysql = require('mysql');
 var table = require('console.table');
+var inquirer = require('inquirer');
 
 //Create new mySQL connection
 var connection = mysql.createConnection({
@@ -19,5 +20,45 @@ connection.connect(function(err){
 
 //Query that displays everything(*) from Products table
 connection.query('SELECT * FROM Products', function(err, res){
-  console.table(res);
+  if (err) throw err;
+  console.table(res); 
+  promptCustomer();
 });
+
+var promptCustomer = function() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "productId",
+      message: "Please enter the itemID for the product you wish to buy."
+    },{
+      type: "input",
+      name: "totalQuantity",
+      message: "Please enter your desired quantity"
+    }
+  ]).then(function(prompt_result){
+      //console.log("You're desired quantity is "+ result.totalQuantity);
+
+      connection.query("SELECT * FROM Products where itemID = '"+prompt_result.productId+"'", function(err, res){
+          if (err) throw err;
+          if (res.length == 0) {
+            console.log("ItemID not found. Please try again.");
+            promptCustomer();
+          }else{
+
+          console.log("\nThe product you've selected is : \n" );
+          console.table(res);
+          console.log("The quantity you've selected is " + prompt_result.totalQuantity)
+          }
+      })
+
+
+      
+
+
+      // var correct = false;
+
+
+
+  })//End of .then
+}//End of function "start"
